faa675e3134270e84b73a1777e1569c9cb7bd292
MauriBian <mauricioosvaldobianchi@gmail.com>
2018-10-18 20:20:16 -0300

    siguen los fix

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -4,10 +4,13 @@ var vidas
 
 func _ready():
 	vidas = get_node("Sprite")
-
+	
 func _process(delta):
 	mostrarVidas()
+	ubicarVidas()
 	
 func mostrarVidas():
 	vidas.play("Vida" + String(GameManager.vidas))
 
+func ubicarVidas():
+	vidas.position.x = get_viewport().size.x - 150
