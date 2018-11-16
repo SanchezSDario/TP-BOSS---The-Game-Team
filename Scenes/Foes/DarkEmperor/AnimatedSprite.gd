8f9c857df8aa6fb31976fa495599b08fd4137cc1
SanchezSDario <dariosebastiansanchez@gmail.com>
2018-11-16 17:36:54 -0300

    Agregado arte para el boss del primero mundo, tambien se le dio comportamiento basico de movimiento por vision, ataque y aniamcion. Falta retocar velocidad de animaciones y ataque ademas del resto de ataques

---
+++ Scenes/Foes/DarkEmperor/AnimatedSprite.gd
@@ -0,0 +1,35 @@
+extends AnimatedSprite
+
+var emperor
+var sprite_animation
+
+func _ready():
+	emperor = get_parent()
+	frames.set_animation_speed("Idle", 12)
+	frames.set_animation_speed("Run", 12)
+	frames.set_animation_speed("Attack", 12)
+	frames.set_animation_speed("Hit", 12)
+	frames.set_animation_speed("Death", 6)
+	frames.set_animation_loop("Attack", false)
+	frames.set_animation_loop("Death", false)
+	frames.set_animation_loop("Hit", false)
+	play("Idle")
+
+func _process(delta):
+	if(!emperor.estoyMuriendo):
+		select_animation()
+		play(sprite_animation)
+	else: play("Death")
+
+func select_animation():
+	match emperor.state_identifier:
+		"WalkRight": 
+			flip_h = false
+			sprite_animation = "Run"
+		"WalkLeft": 
+			flip_h = true
+			sprite_animation = "Run"
+		"Jump": sprite_animation = "Jump"
+		"Attack": sprite_animation ="Attack"
+		"Hit": sprite_animation = "Hit"
+		_: sprite_animation = "Idle"
