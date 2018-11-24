aa649cae9d59b2ba6058115146339327df6e4025
SanchezSDario <dariosebastiansanchez@gmail.com>
2018-11-24 18:04:39 -0300

    Agregado instanciador de pj al primer mapa, tambien se estiró un poco ams y se le agregaron enemigos

--- Scenes/Scenarios/Scenario1/Scenario1.gd
+++ Scenes/Scenarios/Scenario1/Scenario1.gd
@@ -3,9 +3,9 @@ extends Node
 func _ready():
 	var scene_instance = GameManager.personajeActual
 	if scene_instance == null:
-#		scene_instance = load("res://Scenes/PlayableCharacters/Character3/Personaje.tscn")
+		scene_instance = load("res://Scenes/PlayableCharacters/Character3/Personaje.tscn")
 		pass
 	scene_instance = scene_instance.instance()
 	scene_instance.set_name("Player")
-#	add_child(scene_instance)
-	scene_instance.position = Vector2(0,0)
+	add_child(scene_instance)
+	scene_instance.position = Vector2(0,240)
